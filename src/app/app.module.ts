653e4351c1e71a328d5eea74c54bcc4ab731babc
kavitalearns <kavitabansal_01@yahoo.co.in>
2020-11-30 19:21:50 -0600

    proect setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { HeaderComponent } from './header/header.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    HeaderComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule
